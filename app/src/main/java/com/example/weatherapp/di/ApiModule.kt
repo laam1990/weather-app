00107f1e959700fc832949ea72a12f6e50e2462a
Luis Aramburu <laramburu@burntogive.com>
2022-11-27 22:47:53 -0300

    first commit

---
+++ app/src/main/java/com/example/weatherapp/di/ApiModule.kt
@@ -0,0 +1,38 @@
+package com.example.weatherapp.di
+
+import com.example.weatherapp.BuildConfig
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com")
+            .client(getHttpClient())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    private fun getHttpClient(): OkHttpClient {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger.DEFAULT)
+
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            okHttpClientBuilder.addInterceptor(httpLoggingInterceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+}
